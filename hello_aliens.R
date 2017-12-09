b946d45ac61f449864e61a9509ce6f2ae48c9f0d
zhuoyanw <zhuoyanw@andrew.cmu.edu>
2017-12-09 18:52:42 -0500

    Create hello_aliens.R

---
+++ hello_aliens.R
@@ -0,0 +1,9 @@
+# Hello there. It's us, Earth.
+#I want to introduce our planet to you, dear aliens.
+
+# Earth is the 3rd planet from the sun at a distance of 150 million kilometers.
+# It is the only planet in the solar system that is known to produce lives.
+# By the way, we have sent many messages out in order to communicate with you. Did you receive any 
+# of our messages? We would love to receive responses from you.
+
+# Thank you.
